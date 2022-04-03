932eecf692624d4d2cdd4514e3c0c47cd57ab9c1
hhquoc89 <83216140+hhquoc89@users.noreply.github.com>
2022-04-03 19:31:25 +0700

    Update

--- database/ebook.sql
+++ database/ebook.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 05, 2021 at 09:51 AM
--- Server version: 5.7.31
--- PHP Version: 7.3.21
+-- Generation Time: Apr 03, 2022 at 12:30 PM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `full_name`, `username`, `password`) VALUES
+(23, 'admin', 'admin1', 'e00cf25ad42683b3df678c61f42c6bda'),
+(22, 'admin', 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `cart`
 --
 
@@ -32,16 +55,89 @@ CREATE TABLE IF NOT EXISTS `cart` (
   `cart_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `item_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`cart_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=63 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `cart`
+-- Table structure for table `order`
 --
 
-INSERT INTO `cart` (`cart_id`, `user_id`, `item_id`) VALUES
-(14, 1, 4),
-(15, 1, 9);
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE IF NOT EXISTS `order` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `userID` int(11) NOT NULL,
+  `customer_name` varchar(150) DEFAULT NULL,
+  `customer_contact` varchar(20) DEFAULT NULL,
+  `customer_email` varchar(150) DEFAULT NULL,
+  `customer_address` varchar(255) DEFAULT NULL,
+  `total` double DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`id`, `userID`, `customer_name`, `customer_contact`, `customer_email`, `customer_address`, `total`, `status`, `created_date`) VALUES
+(2, 0, 'Huy Quoc', '0964415645', '18521307@gm.uit.edu.vn', 'Vo Thi Sau District 3', 311, 'pending', '2021-11-23 12:24:59'),
+(3, 0, 'Huy Quoc', '0964415645', '18521307@gm.uit.edu.vn', 'Vo Thi Sau District 3', 433, 'pending', '2021-11-23 01:06:12'),
+(4, 0, 'Huy Quoc', '0964415645', '18521307@gm.uit.edu.vn', 'Vo Thi Sau District 3', 396, 'pending', '2021-11-23 01:18:05'),
+(5, 12, 'Tran Huy', NULL, NULL, NULL, 488, 'pending', '2021-12-04 04:17:27'),
+(6, 12, 'Tran Huy', NULL, NULL, NULL, 488, 'pending', '2021-12-04 04:19:21'),
+(7, 12, 'Tran Huy', NULL, NULL, NULL, 488, 'pending', '2021-12-04 04:20:53'),
+(8, 12, 'Tran Huy', NULL, NULL, NULL, 488, 'pending', '2021-12-04 04:21:41'),
+(9, 12, 'Tran Huy', NULL, NULL, NULL, 123, 'pending', '2021-12-10 07:18:22'),
+(10, 12, 'Tran Huy', NULL, NULL, NULL, 123, 'pending', '2021-12-10 11:56:03'),
+(11, 13, 'Tram Nguyen', '', '', '', 492, 'On Delivery', '2021-12-13 00:04:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_item`
+--
+
+DROP TABLE IF EXISTS `order_item`;
+CREATE TABLE IF NOT EXISTS `order_item` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `order_item`
+--
+
+INSERT INTO `order_item` (`id`, `order_id`, `product_id`, `name`, `price`, `qty`, `total`, `created_date`) VALUES
+(1, 2, 12, 'Book1 ', '189.00', 1, '189.00', '2021-11-23 12:24:59'),
+(2, 2, 2, 'Book2', '122.00', 1, '122.00', '2021-11-23 12:24:59'),
+(3, 3, 3, 'Book3', '122.00', 1, '122.00', '2021-11-23 01:06:12'),
+(4, 3, 12, 'Book1 ', '189.00', 1, '189.00', '2021-11-23 01:06:12'),
+(5, 3, 8, 'Book8', '122.00', 1, '122.00', '2021-11-23 01:06:12'),
+(6, 4, 6, 'Book6', '122.00', 1, '122.00', '2021-11-23 01:18:05'),
+(7, 4, 9, 'Book9', '152.00', 1, '152.00', '2021-11-23 01:18:05'),
+(8, 4, 5, 'Book5', '122.00', 1, '122.00', '2021-11-23 01:18:05'),
+(9, 5, 4, 'Book4', '122.00', 1, '122.00', '2021-12-04 04:17:27'),
+(10, 5, 7, 'Book7', '122.00', 1, '122.00', '2021-12-04 04:17:27'),
+(11, 6, 4, 'Book4', '122.00', 1, '122.00', '2021-12-04 04:19:21'),
+(12, 6, 7, 'Book7', '122.00', 1, '122.00', '2021-12-04 04:19:21'),
+(13, 7, 4, 'Book4', '122.00', 1, '122.00', '2021-12-04 04:20:53'),
+(14, 7, 7, 'Book7', '122.00', 1, '122.00', '2021-12-04 04:20:53'),
+(15, 8, 4, 'Book4', '122.00', 1, '122.00', '2021-12-04 04:21:41'),
+(16, 8, 7, 'Book7', '122.00', 1, '122.00', '2021-12-04 04:21:41'),
+(17, 9, 11, 'Book10', '123.00', 1, '123.00', '2021-12-10 07:18:22'),
+(18, 10, 11, 'Book10', '123.00', 1, '123.00', '2021-12-10 11:56:03'),
+(19, 11, 11, 'Book10', '123.00', 4, '492.00', '2021-12-12 03:41:07');
 
 -- --------------------------------------------------------
 
@@ -51,31 +147,31 @@ INSERT INTO `cart` (`cart_id`, `user_id`, `item_id`) VALUES
 
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE IF NOT EXISTS `product` (
-      `item_id` int(11) UNSIGNED  NOT NULL,
-      `item_brand` varchar(200) NOT NULL,
-      `item_name` varchar(255) NOT NULL,
-    `item_description` text NOT NULL,
-      `item_price` double(10,2) NOT NULL,
-      `item_image` varchar(255) NOT NULL,
-      `item_register` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-ALTER TABLE `product` CHANGE `item_id` `item_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT, add PRIMARY KEY (`item_id`);
+  `item_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `item_brand` varchar(200) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `item_description` text NOT NULL,
+  `item_price` double(10,2) NOT NULL,
+  `item_image` varchar(255) NOT NULL,
+  `item_register` datetime DEFAULT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`item_id`, `item_brand`, `item_name`,`item_description`, `item_price`, `item_image`, `item_register`) VALUES
-(1, '1', 'Book1','a', 152.00, './assets/book-1.png', '2020-03-28 11:08:57'),
-(2, '1', 'Book2','a', 122.00, './assets/book-2.png', '2020-03-28 11:08:57'),
-(3, '1', 'Book3','a', 122.00, './assets/book-3.png', '2020-03-28 11:08:57'),
-(4, '2', 'Book4','a', 122.00, './assets/book-4.png', '2020-03-28 11:08:57'),
-(5, '2', 'Book5','a', 122.00, './assets/book-5.png', '2020-03-28 11:08:57'),
-(6, '2', 'Book6','a', 122.00, './assets/book-6.png', '2020-03-28 11:08:57'),
-(7, '2', 'Book7','a', 122.00, './assets/book-7.png', '2020-03-28 11:08:57'),
-(8, '3', 'Book8','a', 122.00, './assets/book-8.png', '2020-03-28 11:08:57'),
-(9, '3', 'Book9','a', 152.00, './assets/book-9.png', '2020-03-28 11:08:57'),
-(10, '3', 'Book10','a', 152.00, './assets/book-10.png', '2020-03-28 11:08:57');
+INSERT INTO `product` (`item_id`, `item_brand`, `item_name`, `item_description`, `item_price`, `item_image`, `item_register`) VALUES
+(2, '1', 'Book2', 'a', 122.00, './assets/book-2.png', '2020-03-28 11:08:57'),
+(3, '1', 'Book3', 'a', 122.00, './assets/book-3.png', '2020-03-28 11:08:57'),
+(4, '2', 'Book4', 'a', 122.00, './assets/book-4.png', '2020-03-28 11:08:57'),
+(5, '2', 'Book5', 'a', 122.00, './assets/book-5.png', '2020-03-28 11:08:57'),
+(6, '2', 'Book6', 'a', 122.00, './assets/book-6.png', '2020-03-28 11:08:57'),
+(7, '2', 'Book7', 'a', 122.00, './assets/book-7.png', '2020-03-28 11:08:57'),
+(8, '3', 'Book8', 'a', 122.00, './assets/book-8.png', '2020-03-28 11:08:57'),
+(9, '3', 'Book9', 'a', 152.00, './assets/book-9.png', '2020-03-28 11:08:57'),
+(11, '3', 'Book10', 'dsadsa', 123.00, './assets/Book-1637636152.png', '2021-11-23 09:55:00'),
+(12, '1', 'Book1 ', 'dasd', 189.00, './assets/Book-1637641199.png', '2021-11-23 11:19:00');
 
 -- --------------------------------------------------------
 
@@ -85,11 +181,26 @@ INSERT INTO `product` (`item_id`, `item_brand`, `item_name`,`item_description`, 
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` int(11) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `register_date` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(100) DEFAULT NULL,
+  `lastName` varchar(100) DEFAULT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(150) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `password` varchar(200) NOT NULL,
+  `profileImage` varchar(255) DEFAULT NULL,
+  `registerDate` datetime DEFAULT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userID`, `firstName`, `lastName`, `email`, `contact`, `address`, `password`, `profileImage`, `registerDate`) VALUES
+(11, 'Quoc', 'Huy', '18521307@gm.uit.edu.vn', '0964415645', 'Vo Thi Sau District 3 ', '$2y$10$sLrq/hJmVFdenhrWaOyWSuWJ8vbhTlt1mZNCbJDBYZxM349DBLCMG', './assets/profile/beard.png', '2021-11-24 03:48:32'),
+(12, 'Tran', 'Huy', 'steve_opencart@yahoo.com', '0964415645', 'Vo Thi Sau District 3 ', 'e10adc3949ba59abbe56e057f20f883e', './assets/profile/beard.png', '2021-12-04 03:13:43'),
+(13, 'Tram', 'Nguyen', 'tramnguyen123@gmail.com', '123456789', 'Vo Thi Sau District 4 ', '89a1a868823abe7243fd340554055ce5', './assets/profile/pic-2.png', '2021-12-10 11:00:01');
 
 -- --------------------------------------------------------
 
@@ -101,18 +212,11 @@ DROP TABLE IF EXISTS `wishlist`;
 CREATE TABLE IF NOT EXISTS `wishlist` (
   `cart_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL
+  `item_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
-                                           `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-                                           `full_name` varchar(100) NOT NULL,
-    `username` varchar(100) NOT NULL,
-    `password` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`)
-    ) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
